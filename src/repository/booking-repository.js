f9d5ab6ae33915fd8952942691dad8206c3a55aa
Hari Shanker Yadav <113604005+Harishankeryadav@users.noreply.github.com>
2024-03-09 15:11:13 +0530

    removed but and addded services

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -1,27 +1,41 @@
-const {StatusCodes} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 
-const{Booking} = require('../models/index');
-const{AppError, ValidationError}= require('../utiles/errors/index');
+const { Booking } = require('../models/index');
+const { AppError, ValidationError } = require('../utiles/errors/index');
 
-class BookingRepository{
-    async create(data){
+class BookingRepository {
+    async create(data) {
         try {
             const booking = await Booking.create(data);
             return booking;
         } catch (error) {
-            if(error.name = 'sequelizeValidationError'){
+            if(error.name == 'SequelizeValidationError') {
                 throw new ValidationError(error);
             }
-            throw new AppError('RepositoryError',
-            'Cannot create Booking',
-            'there was some issue try later',
-            statusCodes.INTERNAL_SERVER_ERROR);
+            throw new AppError(
+                'RepositoryError', 
+                'Cannot create Booking', 
+                'There was some issue creating the booking, please try again later',
+                StatusCodes.INTERNAL_SERVER_ERROR);
         }
     }
-    async update(data){
-        
-    }
 
+    async update(bookingId, data) {
+        try {
+            const booking = await Booking.findByPk(bookingId);
+            if(data.status) {
+                booking.status = data.status;
+            }
+            await booking.save();
+            return booking;
+        } catch (error) {
+            throw new AppError(
+                'RepositoryError', 
+                'Cannot update Booking', 
+                'There was some issue updating the booking, please try again later',
+                StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
 
 module.exports = BookingRepository;
--- src/utiles/errors/service-error.js
+++ src/utiles/errors/service-error.js
@@ -1,16 +1,17 @@
-const {statusCodes} = require('http-status-codes');
+const { statusCodes } = require('http-status-codes');
 
-class  serviceError extends Error {
-    constructor (  message,
-        explanation,
+class serviceError extends Error {
+    constructor(
+        message = 'something went wrong',
+        explanation = 'Service layer error',
         statusCode = statusCodes.INTERNAL_SERVER_ERROR
     ) {
         super();
         this.name = 'ServiceError';
-       this.message = message;
-       this.explanation = explanation;
-       this.statusCode = statusCode;
-      }
+        this.message = message;
+        this.explanation = explanation;
+        this.statusCode = statusCode;
+    }
 }
 
 module.exports = serviceError;
